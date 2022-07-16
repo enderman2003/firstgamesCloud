5fe993854f5a85da6dae9322206d7bb5e71496ba
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2022-07-16 13:13:25 +0530

    Update verify_email.lua

--- data/modules/verify_email.lua
+++ data/modules/verify_email.lua
@@ -27,15 +27,15 @@ local function verify_email(context, payload)
 	
 
 	local content = {
-          "to: [
-		{  
-	       		email: email
-		}
-	        ],
-        	templateId: 1,
-		params: { 
- 			OTP: math.random(000000, 999999),
-		}"
+          	[[to: [
+			{  
+				email: email
+			}
+			],
+			templateId: 1,
+			params: { 
+				OTP: math.random(000000, 999999),
+		}]]
 	}
 	local method = "POST"
 	local headers = {

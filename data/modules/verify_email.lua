519832c72f11ba2e4b9686adf52c6b1788e32b3f
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2022-07-16 13:28:28 +0530

    Update verify_email.lua

--- data/modules/verify_email.lua
+++ data/modules/verify_email.lua
@@ -3,24 +3,11 @@ local nk = require("nakama")
 local HTTPS_PREFIX = "https://"
 local SENDINBLUE_URL = "https://api.sendinblue.com/v3/smtp/email"
 
-local function verify_email(context, payload)
+local function verify_email(_)
 
 	local http_key = "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"
 
 	local url = string.format(SENDINBLUE_URL, HTTPS_PREFIX)
-
-	local json_payload = nk.json_decode(payload)
-	local email = json_payload.email
-
-	local query = [[SELECT id FROM users WHERE email = $1 LIMIT 1]]
-	local query_result = nk.sql_query(query, { email })
-
-	if next(query_result) == nil then
-		return nk.json_encode({
-			["success"] = false,
-			["error"] = "Email does not exist!"
-		})
-	end
 
 	local user_id = query_result[1].id
 

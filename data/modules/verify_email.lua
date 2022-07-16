09daff9c235a9bdb1174cf256cd016def9b740f1
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2022-07-16 17:23:18 +0530

    Update verify_email.lua

--- data/modules/verify_email.lua
+++ data/modules/verify_email.lua
@@ -12,7 +12,7 @@ local function verify_email(_, payload)
 	local method = "POST"
 	local headers = {
 		["Content-Type"] = "application/json",
-   		["api-key"] = "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",
+   		["api-key"] = "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",
 	}
 
 	local success, code, headers, body = pcall(nk.http_request, url, method, headers, content)

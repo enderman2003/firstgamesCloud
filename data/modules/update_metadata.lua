891bd20101d4df3e0807de1ef4eece0021c0c5ea
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2022-07-17 21:08:20 +0530

    Update update_metadata.lua

--- data/modules/update_metadata.lua
+++ data/modules/update_metadata.lua
@@ -2,11 +2,11 @@ local nk = require('nakama')
 local function update_metadata(context, payload)
     local userId = context.user_id
     local data = nk.json_decode(payload).data
-    if userId == nil then
+    if (userId == nil) then
         error("Data could not be updated")
     end
 
-    local update = nk.account_update_id(user_id, data)
+    local update = nk.account_update_id(userId, data)
     return payload
 end
 nk.register_rpc(update_metadata, "update_metadata")

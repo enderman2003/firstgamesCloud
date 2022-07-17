6e4d43e89b2643c48af106fa68246d0a63721c91
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2022-07-17 20:13:15 +0530

    Update update_metadata.lua

--- data/modules/update_metadata.lua
+++ data/modules/update_metadata.lua
@@ -3,10 +3,11 @@ local function update_metadata(context, payload)
     local userId = nk.json_decode(payload).user_id
     local data = nk.json_decode(payload).data
     if userId == null then
-        return "Data could not be updated"
+        error("Data could not be updated")
     end
 
     local update = nk.AccountUpdateId(_, userId, data)
+    return payload
 end
 nk.register_rpc(update_metadata, "update_metadata")
 

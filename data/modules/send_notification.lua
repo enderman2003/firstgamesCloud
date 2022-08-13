cebb3ab2cd9344fd5637d68e299b3d064a615611
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2022-08-13 15:22:23 +0530

    Update send_notification.lua

--- data/modules/send_notification.lua
+++ data/modules/send_notification.lua
@@ -30,4 +30,20 @@ local function send_notification(context, payload)
 
   nk.notification_send(new_notifications)
 end
+
+local function send_notification_fr(context, payload)
+  local data = nk.json_decode(payload)
+  if (context.user_id == nil) then
+    error("Data could not be updated")
+  end
+  local sender_id = context.user_id,
+  local content = data.content,
+  local user_id = data.p2_id,
+  local code = data.code,
+  local persistent = true
+
+  nk.notification_send(user_id, nil, content, code, persistent)
+end
+
 nk.register_rpc(send_notification, "send_notification")
+nk.register_rpc(send_notification_fr, "send_notification_fr")

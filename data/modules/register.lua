93d5c572f03e98b69e4ee73afc0dae9157d7837f
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-02-15 14:15:36 +0530

    Update register.lua

--- data/modules/register.lua
+++ data/modules/register.lua
@@ -11,4 +11,19 @@ local function create_user(context, payload)
     nk.storage_write(new_objects)
     return payload
 end
+local nk = require("nakama")
+
+local function send_location(context, payload)
+    local userId = context.user_id
+    local data = nk.json_decode(payload)
+
+    local new_objects = {
+       { collection = userId, key = "user_data", user_id = userId, value = {value=data['data']}, version="*", permission_read = 1, permission_write = 1 },
+    }
+
+    nk.storage_write(new_objects)
+    return payload
+end
+
+nk.register_rpc(send_location, "send_location")
 nk.register_rpc(create_user, "create_user")

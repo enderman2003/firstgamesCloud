a0fad4497f8d5106f0cabf4801c49b0a2fa7ac6b
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-02-15 19:29:16 +0530

    Update register.lua

--- data/modules/register.lua
+++ data/modules/register.lua
@@ -9,39 +9,7 @@ local function create_user(context, payload)
             key = userId, 
             user_id = userId, 
             value = {
-                data={
-                    kills=data['data']['kills'],
-                    deaths=data['data']['deaths'],
-                    ability=data['data']['ability'],
-                    cool_down=data['data']['cool_down'],
-                    cp=data['data']['cp'],
-                    curr_weapon=data['data']['curr_weapon'],
-                    location={
-                        lon=data['data']['lon'],
-                        lat=data['data']['lat'],
-                        ip_addr=data['data']['location']['ip_addr'],
-                        country=data['data']['location']['country'],
-                        region=data['data']['location']['region'],
-                        city=data['data']['location']['city']
-                    },
-                    ban_time={
-                        day=data['data']['ban_time']['day'],
-                        month=data['data']['ban_time']['month'],
-                        year=data['data']['ban_time']['year'],
-                        hours=data['data']['ban_time']['hours'],
-                        minutes=data['data']['ban_time']['minutes']
-                    },
-                    weapon_inv={
-                        Hands={
-                            level=data['data']['weapon_inv']['Hands']['level'],
-                            cp=data['data']['weapon_inv']['Hands']['cp']
-                        }
-                    },
-                    upgrade_inv={
-                        banadges=data['data']['upgrade_inv']['bandages'],
-                        punching_bag=data['data']['upgrade_inv']['punching_bag']
-                    }
-                }
+                data=data.data
             },
             permission_read = 2, 
             permission_write = 1

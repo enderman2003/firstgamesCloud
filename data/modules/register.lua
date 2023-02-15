b5dbf5143042c46b83e0d9411d3d20a2938f5903
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-02-15 16:13:55 +0530

    Update register.lua

--- data/modules/register.lua
+++ data/modules/register.lua
@@ -6,8 +6,8 @@ local function create_user(context, payload)
 
     local new_objects = {
        { 
-            collection = userId, 
-            key = "user_data", 
+            collection = "user_data", 
+            key = userId, 
             user_id = userId, 
             value = {data=data['data']},
             version = "*",
@@ -20,24 +20,4 @@ local function create_user(context, payload)
     return payload
 end
 
-local function send_location(context, payload)
-    local userId = context.user_id
-    local data = nk.json_decode(payload)
-
-    local new_objects = {
-       { 
-            collection = userId, 
-            key = "user_data", 
-            user_id = userId, 
-            value = {location=data['data']},
-            version = "*",
-            permission_read = 1, 
-            permission_write = 1 },
-    }
-
-    nk.storage_write(new_objects)
-    return payload
-end
-
-nk.register_rpc(send_location, "send_location")
 nk.register_rpc(create_user, "create_user")

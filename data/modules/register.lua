070ade98853098ae91a7f184fcd89b553d7c03eb
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-02-15 18:53:42 +0530

    Update register.lua

--- data/modules/register.lua
+++ data/modules/register.lua
@@ -9,38 +9,7 @@ local function create_user(context, payload)
             key = userId, 
             user_id = userId, 
             value = {
-                data={
-                    kills=data.kills,
-                    deaths=data.deaths,
-                    ability=data.ability,
-                    cool_down=data.cool_down,
-                    cp=data.cp,
-                    curr_weapon=data.curr_weapon,
-                    location={
-                        lon=data.lon,
-                        lat=data.lat,
-                        ip_addr=data['location'].ip_addr,
-                        country=data['location'].country,
-                        region=data['location'].region,
-                        city=data['location'].city
-                    },
-                    ban_time={
-                        day=data['ban_time'].day,
-                        month=data['ban_time'].month,
-                        year=data['ban_time'].year,
-                        hours=data['ban_time'].hours,
-                        minutes=data['ban_time'].minutes
-                    },
-                    weapon_inv={
-                        Hands={
-                            level=data['weapon_inv']['Hands'].level,
-                            cp=data['weapon_inv']['Hands'].cp
-                        }
-                    },
-                    upgrade_inv={
-                        banadges=data['upgrade_inv'].bandages,
-                        punching_bag=data['upgrade_inv'].punching_bag
-                    }
+                data=data
             },
             permission_read = 2, 
             permission_write = 1 

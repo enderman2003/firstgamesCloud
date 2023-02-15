7a5fb37b4e5a886b77ba7e910300218697fba8d2
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-02-15 13:48:16 +0530

    Update register.lua

--- data/modules/register.lua
+++ data/modules/register.lua
@@ -5,17 +5,7 @@ local function create_user(context, payload)
     local data = nk.json_decode(payload)
 
     local new_objects = {
-       { collection = userId, key = "kills", user_id = userId, value = {value=data['kills']}, version="*", permission_read = 1, permission_write = 1 },
-       { collection = userId, key = "deaths", user_id = userId, value = {value=data['deaths']}, version="*", permission_read = 1, permission_write = 1 },
-       { collection = userId, key = "ability", user_id = userId, value = {value=data['ability']}, version="*", permission_read = 1, permission_write = 1 },
-       { collection = userId, key = "lon", user_id = userId, value = {value=data['lon']}, version="*", permission_read = 1, permission_write = 1 },
-       { collection = userId, key = "lat", user_id = userId, value = {value=data['lat']}, version="*", permission_read = 1, permission_write = 1 },
-       { collection = userId, key = "ban_time", user_id = userId, value = {value= data['ban_time']}, version="*", permission_read = 1, permission_write = 1 },
-       { collection = userId, key = "curr_weapon", user_id = userId, value = {value=data['curr_weapon']}, version="*", permission_read = 1, permission_write = 1 },
-       { collection = userId, key = "cooldown", user_id = userId, value = {value=data['cool_down']}, version="*", permission_read = 1, permission_write = 1 },
-       { collection = userId, key = "cp", user_id = userId, value = {value=data['cp']}, version="*", permission_read = 1, permission_write = 1 },
-       { collection = userId, key = "upgrade_inv", user_id = userId, value = {value=data['upgrade_inv']}, version="*", permission_read = 1, permission_write = 1 },
-       { collection = userId, key = "weapon_inv", user_id = userId, value = {value=data['weapon_inv']}, version="*", permission_read = 1, permission_write = 1 }
+       { collection = userId, key = "user_data", user_id = userId, value = {value=data['data']}, version="*", permission_read = 1, permission_write = 1 },
     }
 
     nk.storage_write(new_objects)

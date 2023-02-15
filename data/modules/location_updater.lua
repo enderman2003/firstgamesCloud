e474799bc2b6c39849a73bf01e0cecd6bc93791f
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-02-15 14:25:28 +0530

    Update and rename world_rpc.lua to location_updater.lua

---
+++ data/modules/location_updater.lua
@@ -0,0 +1,26 @@
+local nakama = require("nakama")
+
+local function location_update(context, payload)
+    local userId = context.user_id
+    local data = nk.json_decode(payload)
+
+    local new_objects = {
+       { 
+            collection = userId, 
+            key = "user_data", 
+            user_id = userId, 
+            value = {
+                location={
+                    lat=data['lat']
+                    lon=data['lon']
+                }
+            }, 
+            permission_read = 1, 
+            permission_write = 1 },
+    }
+
+    nk.storage_write(new_objects)
+    return payload
+end
+
+nk.register_rpc(location_update, "location_update")

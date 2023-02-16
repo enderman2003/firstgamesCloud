2d13babd54eac6ec3e145da4aa969d3da4ef35d1
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-02-16 20:54:31 +0530

    Update update_wallet.lua

--- data/modules/update_wallet.lua
+++ data/modules/update_wallet.lua
@@ -14,5 +14,23 @@ local function update_wallet(context, payload)
     local update = nk.wallet_update(userId, changeset, metadata, true)
     return payload
 end
+
+local function add_coins(context, payload)
+    local userId = context.user_id
+    local data = nk.json_decode(payload)
+    if (userId == nil) then
+        error("Data could not be updated")
+    end
+    local changeset = {
+        coins = data.coins
+    }
+    local metadata = {
+        game_result = "killed"
+    }
+    local update = nk.wallet_update(userId, changeset, metadata, true)
+    return payload
+end
+
+nk.register_rpc(add_coins, "add_coins")
 nk.register_rpc(update_wallet, "update_wallet")
 

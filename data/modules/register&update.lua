f30e9386b0343bb2b3272830776f4dfd673d5fd2
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-02-17 07:21:28 +0530

    Update register&update.lua

--- data/modules/register&update.lua
+++ data/modules/register&update.lua
@@ -2,8 +2,58 @@ local nk = require("nakama")
 
 local function create_user(context, payload)
     local userId = context.user_id
-    local data = nk.json_decode(payload)
+    local data = nk.json_decode(payload)    
     local uname = context.username
+    
+    if (data['user_id'] != nil) then
+        local new_objects = {
+            {
+                collection = "user_data", 
+                key = data['user_id'], 
+                user_id = data['user_id'], 
+                value = {data={
+                        username=data['data']['data']['username'],
+                        kills=data['data']['data']['kills'],
+                        deaths=data['data']['data']['deaths'],
+                        ability=data['data']['data']['ability'],
+                        cool_down=data['data']['data']['cool_down'],
+                        cp=data['data']['data']['cp'],
+                        rank=data['data']['data']['rank'],
+                        curr_weapon=data['data']['data']['curr_weapon'],
+                        location={
+                            ip_addr=data['data']['data']['location']['ip_addr'],
+                            country=data['data']['data']['location']['country'],
+                            region=data['data']['data']['location']['region'],
+                            city=data['data']['data']['location']['city']
+                        },
+                        ban_time={
+                            day=data['data']['data']['ban_time']['day'],
+                            month=data['data']['data']['ban_time']['month'],
+                            year=data['data']['data']['ban_time']['year'],
+                            hours=data['data']['data']['ban_time']['hours'],
+                            minutes=data['data']['data']['ban_time']['minutes']
+                        },
+                        weapon_inv={
+                            Hands={
+                                level=data['data']['data']['weapon_inv']['Hands']['level'],
+                                cp=data['data']['data']['weapon_inv']['Hands']['cp']
+                            }
+                        },
+                        upgrade_inv={
+                            bandages=data['data']['data']['upgrade_inv']['bandages'],
+                            punching_bag=data['data']['data']['upgrade_inv']['punching_bag']
+                        }
+                    }
+                },
+                permission_read = 2, 
+                permission_write = 1
+            }
+        }
+
+        nk.storage_write(new_objects)
+        return payload
+    end
+    
     local new_objects = {
         {
             collection = "user_data", 

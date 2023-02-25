5049e8daf291b9c93b6e024f8a2067e0726651a2
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-02-26 05:15:26 +0530

    Update world_rpc_id.lua

--- data/modules/world_rpc_id.lua
+++ data/modules/world_rpc_id.lua
@@ -5,7 +5,7 @@ local function _get_first_world()
     local current_match = matches[1]
 
     if current_match == nil then
-        return nakama.match_create("world_control", {})
+        return nakama.match_create("world_rpc", {})
     else
         return current_match.match_id
     end

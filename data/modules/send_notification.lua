37d0404a5713f407b72213ebf2413c9c9d0758b4
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-02-26 07:09:13 +0530

    Update send_notification.lua

--- data/modules/send_notification.lua
+++ data/modules/send_notification.lua
@@ -4,60 +4,18 @@ local function send_notification(context, payload)
   if (context.user_id == nil) then
     error("Data could not be updated")
   end
-  local new_notifications = {
-    {
-      sender_id = context.user_id,
-      content = data.content,
-      user_id = data.p2_id,
-      code = data.code,
-      persistent = true
-    },
-    {
-      sender_id = context.user_id,
-      content = data.content,
-      user_id = data.data.p3_id,
-      code = data.code,
-      persistent = true
-    },
-    {
-      sender_id = context.user_id,
-      content = data.content,
-      user_id = data.p4_id,
-      code = data.code,
-      persistent = true
-    }
+  local subject = "Locked in battle"
+
+  local content = {
+    matched = true
   }
 
-  nk.notification_send(new_notifications)
-end
+  local receiver_id = data.data.rem_id
+  local sender_id = context.user_id
+  local code = 101
+  local persistent = false
 
-local function send_notification_fr(context, payload)
-  local data = nk.json_decode(payload)
-  if (context.user_id == nil) then
-    error("Data could not be updated")
-  end
-  if (data.code == 102) then
-    local sender_id = context.user_id
-    local content = {
-      message = "Friend request from " .. context.username
-    }
-    local user_id = data.id
-    local code = data.code
-    local persistent = true
-    nk.notification_send(user_id, nil, content, code, sender_id, persistent)
-  end
-  if (data.code == 103) then
-    local sender_id = context.user_id
-    local content = {
-      message = "Invitation from " .. context.username,
-      match_id = data.match_id
-    }
-    local user_id = data.id
-    local code = data.code
-    local persistent = true
-    nk.notification_send(user_id, nil, content, code, sender_id, persistent)
-  end
-end
+  nk.notification_send(receiver_id, subject, content, code, sender_id, persistent)
+end 
 
 nk.register_rpc(send_notification, "send_notification")
-nk.register_rpc(send_notification_fr, "send_notification_fr")

338409fd6d98b9e5e9bb983f3a065e75caafbefd
Sahil Raut <78951457+enderman2003@users.noreply.github.com>
2023-02-26 22:30:28 +0530

    Update world_rpc_id.lua

--- data/modules/world_rpc_id.lua
+++ data/modules/world_rpc_id.lua
@@ -2,7 +2,7 @@ local nakama = require("nakama")
 
 local function _get_first_world()
     local matches = nakama.match_list()
-    local current_match = matches[0]
+    local current_match = matches[1]
 
     if current_match == nil then
         return nakama.match_create("world_control", {})

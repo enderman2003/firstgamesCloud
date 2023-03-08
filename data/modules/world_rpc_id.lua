923c074d03a3cf33137b04ce1ec49ea7ff5a62a4
Sahil Raut <neocrust98@gmail.com>
2023-03-08 22:37:54 +0530

    Update world_rpc_id.lua

--- data/modules/world_rpc_id.lua
+++ data/modules/world_rpc_id.lua
@@ -1,14 +1,42 @@
-local nakama = require("nakama")
+local nk = require("nakama")
 
-local function send_to_battle()
-    local matches = nakama.match_list()
-    local current_match = matches[1]
+local function death()
+  local data = nk.json_decode(payload)
+  if (context.user_id == nil) then
+    error("Data could not be updated")
+  end
+  local subject = "Dead"
 
-    if current_match == nil then
-        return nakama.match_create("world_control", {})
-    else
-        return current_match.match_id
-    end
+  local content = {
+    opponent_dead = true
+  }
+
+  local receiver_id = data.data.rem_id
+  local sender_id = context.user_id
+  local code = 153
+  local persistent = false
+
+  nk.notification_send(receiver_id, subject, content, code, sender_id, persistent)
 end
 
-nakama.register_rpc(send_to_battle, "send_to_battle")
+local function eliminated()
+  local data = nk.json_decode(payload)
+  if (context.user_id == nil) then
+    error("Data could not be updated")
+  end
+  local subject = "Eliminated"
+
+  local content = {
+    opponent_win = true
+  }
+
+  local receiver_id = data.data.rem_id
+  local sender_id = context.user_id
+  local code = 156
+  local persistent = false
+
+  nk.notification_send(receiver_id, subject, content, code, sender_id, persistent)
+end
+
+nk.register_rpc(death, "death")
+nk.register_rpc(eliminated, "eliminated")
